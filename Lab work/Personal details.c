b9d74879561131c2968058cb9da6de67af567c6f
Prashant95860 <115604751+Prashant95860@users.noreply.github.com>
2022-10-20 22:10:47 +0530

    Create Personal details.c

---
+++ Lab work/Personal details.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main()
+{
+ printf("Name   : Prashant Shukla");"/n"
+ 
+ 
+ printf("D.O.B  : 28/04/2002:");
+ 
+ printf("Gender : Male:");
+ 
+ printf("Address: Ahmedabad:");
+}
