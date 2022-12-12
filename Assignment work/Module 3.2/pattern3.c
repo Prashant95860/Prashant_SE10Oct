3c497a73aee4b2235be26e1c3ca493c1736aa51f
Prashant95860 <115604751+Prashant95860@users.noreply.github.com>
2022-12-12 08:55:02 +0530

    Create pattern3.c

---
+++ Assignment work/Module 3.2/pattern3.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+	int i,j;
+	for(i=0;i<5;i++)
+	{
+	
+		for(j=0;j<5+i;j++)
+		{
+		
+			if(j<5-i-1)
+	{
+		printf("");
+		
+	}
+	else
+	{
+		printf("* ");
+		
+	}
+		
+		printf("\n");
+	}
+}
+}
+	
+
+
+
+
